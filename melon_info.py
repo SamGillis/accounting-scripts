3d0e4681b5be389772233e89d803990ef45a35bf
Sam Gillis <sbvb13@gmail.com>
2020-10-06 01:03:43 +0000

    Finished prompt

--- melon_info.py
+++ melon_info.py
@@ -1,11 +1,24 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
+def print_all_melons(melons):
+    """Print every melon and corresponding attributes"""
+
+    for melon_name, attributes in melons.items():
+        print(melon_name.upper())
+
+        for attribute, value in attributes.items():
+            print(f'{attribute}: {value}')
+
+        print('==================')
+
+
+
+"""def print_melon(name, seedless, price):
+    'Print each melon with corresponding attribute information.'
 
     have_or_have_not = 'have'
     if seedless:
@@ -15,4 +28,4 @@ def print_melon(name, seedless, price):
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])"""
